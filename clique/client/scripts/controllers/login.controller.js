d3590276a4f594d45459c4de0f85d45814b31c2d
Michael Tobin <mikel.tobin@gmail.com>
2016-09-07 16:09:21 -0600

    Login controller and is configured

---
+++ clique/client/scripts/controllers/login.controller.js
@@ -0,0 +1,12 @@
+import Ionic from 'ionic-scripts';
+import { _ } from 'meteor/underscore';
+import { Meteor } from 'meteor/meteor';
+import { Controller } from 'angular-ecmascript/module-helpers';
+
+
+export default class LoginCtrl extends Controller {
+  constructor() {
+    super(...arguments);
+  }
+}
+LoginCtrl.$inject = ['$stateParams', '$timeout', '$ionicScrollDelegate'];
